b91a4c15389d8cc9417e0f5df7b3021b34ced8f2
wilsonriosv <wilsonriosv@gmail.com>
2023-03-29 21:19:15 -0500

    otro ajuste
    otro ajuste

---
+++ js/pruebas.js
@@ -0,0 +1,15 @@
+let arr = [2, 3, 5, 7]
+
+arr.map(function(element, index, array){
+	console.log(this) // 80
+}, 7);
+
+
+let arr2 = [2, 3, 5, 7]
+
+arr.map(function(element, index, array){
+    console.log(element);
+    console.log(index);
+    console.log(array);
+    return element;
+}, 80);
